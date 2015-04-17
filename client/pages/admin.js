de1e08d045b55bafc53ae6f26bc5cb230b576601
Mike Cornish <cornishmw@gmail.com>
2015-04-17 18:11:53 -0400

    Admin table sorting

--- client/pages/admin.js
+++ client/pages/admin.js
@@ -10,6 +10,9 @@ var GiftListView = require('../views/giftList')
 var Firebase = require('firebase');
 var ref = new Firebase('sizzling-fire-6725.firebaseIO.com');
 
+var sort = '';
+var reverse = false;
+
 
 module.exports = PageView.extend({
     pageTitle: 'Admin Dashboard',
@@ -47,51 +50,141 @@ module.exports = PageView.extend({
     },
     sortByTitle: function() {
 
-        sortTable(this, function(gifts) {
-            gifts.sort(function(a, b) {
-                if (a.title > b.title) {
-                    return 1;
-                }
-                if (a.title < b.title) {
-                    return -1;
-                }
-                return 0;
+        checkReverse('title');
+
+        if (reverse === true) {
+            sortTable(this, function(gifts) {
+                gifts.sort(function(a, b) {
+                    if (a.title < b.title) {
+                        return 1;
+                    }
+                    if (a.title > b.title) {
+                        return -1;
+                    }
+                    return 0;
+                });
             });
-        });
+
+            hideArrows(this);
+            this.query('[data-hook="header-title"] .arrow-up').style.display = 'inline-block';
+        } else {
+            sortTable(this, function(gifts) {
+                gifts.sort(function(a, b) {
+                    if (a.title > b.title) {
+                        return 1;
+                    }
+                    if (a.title < b.title) {
+                        return -1;
+                    }
+                    return 0;
+                });
+            });
+
+            hideArrows(this);
+            this.query('[data-hook="header-title"] .arrow-down').style.display = 'inline-block';
+        }
+
+        sort = 'title';
 
     },
     sortByUser: function() {
 
-        sortTable(this, function(gifts) {
-            gifts.sort(function(a, b) {
-                if (a.user > b.user) {
-                    return 1;
-                }
-                if (a.user < b.user) {
-                    return -1;
-                }
-                return 0;
+        checkReverse('user');
+
+        if (reverse === true) {
+            sortTable(this, function(gifts) {
+                gifts.sort(function(a, b) {
+                    if (a.user < b.user) {
+                        return 1;
+                    }
+                    if (a.user > b.user) {
+                        return -1;
+                    }
+                    return 0;
+                });
             });
-        });
+
+            hideArrows(this);
+            this.query('[data-hook="header-user"] .arrow-up').style.display = 'inline-block';
+        } else {
+            sortTable(this, function(gifts) {
+                gifts.sort(function(a, b) {
+                    if (a.user > b.user) {
+                        return 1;
+                    }
+                    if (a.user < b.user) {
+                        return -1;
+                    }
+                    return 0;
+                });
+            });
+
+            hideArrows(this);
+            this.query('[data-hook="header-user"] .arrow-down').style.display = 'inline-block';
+        }
+
+        sort = 'user';
 
     },
     sortByCreated: function() {
 
-        sortTable(this, function(gifts) {
-            gifts.sort(function(a, b) {
-                return b.created - a.created;
-            });
-        });
+        checkReverse('created');
 
+        if (reverse === true) {
+
+            sortTable(this, function(gifts) {
+                gifts.sort(function(a, b) {
+                    return a.created - b.created;
+                });
+            });
+
+            hideArrows(this);
+            this.query('[data-hook="header-created"] .arrow-up').style.display = 'inline-block';
+
+        } else {
+
+            sortTable(this, function(gifts) {
+                gifts.sort(function(a, b) {
+                    return b.created - a.created;
+                });
+            });
+
+            hideArrows(this);
+            this.query('[data-hook="header-created"] .arrow-down').style.display = 'inline-block';
+
+        }
+
+        sort = 'created';
     },
     sortByFeatured: function() {
 
-        sortTable(this, function(gifts) {
-            gifts.sort(function(a, b) {
-                return b.featured - a.featured;
-            });
-        });
+        checkReverse('featured');
 
+        if (reverse === true) {
+
+            sortTable(this, function(gifts) {
+                gifts.sort(function(a, b) {
+                    return a.featured - b.featured;
+                });
+            });
+
+            hideArrows(this);
+            this.query('[data-hook="header-featured"] .arrow-up').style.display = 'inline-block';
+
+        } else {
+
+            sortTable(this, function(gifts) {
+                gifts.sort(function(a, b) {
+                    return b.featured - a.featured;
+                });
+            });
+
+            hideArrows(this);
+            this.query('[data-hook="header-featured"] .arrow-down').style.display = 'inline-block';
+
+        }
+
+        sort = 'featured';
     }
 });
 
@@ -104,7 +197,7 @@ var getOrFetchCollection = function(collection, callback) {
     } else {
         callback();
     }
-}
+};
 
 var sortTable = function(self, sortFunction) {
     self.queryByHook('table-view').innerHTML = '';
@@ -118,4 +211,22 @@ var sortTable = function(self, sortFunction) {
     });
 
     self.renderCollection(giftsCollection, GiftListView, self.queryByHook('table-view'));
-}
+};
+
+var checkReverse = function(header) {
+    if (sort === header) {
+        reverse = !reverse;
+    } else {
+        reverse = false;
+    }
+};
+
+var hideArrows = function(self) {
+    self.queryAll('.arrow-down').forEach(function(arrow) {
+        arrow.style.display = 'none';
+    });
+
+    self.queryAll('.arrow-up').forEach(function(arrow) {
+        arrow.style.display = 'none';
+    });
+};
