fa06fe298ffd984b23a9b23a1476b73df772fc32
Mike Cornish <cornishmw@gmail.com>
2015-04-18 10:45:25 -0400

    Basic functional test

--- test/spacemonkey/functionalTests.js
+++ test/spacemonkey/functionalTests.js
@@ -4,32 +4,28 @@
 monkey.loadApp('/', {
     height: 500,
     width: 600,
-    bugUrl: 'https://github.com/henrikjoreteg/humanjs-sample-app/issues/new'
+    bugUrl: 'https://github.com/mCornish/reacTrack/issues/new'
 });
 
-test('basic app experience', function () {
+test('QA Primary', function () {
     monkey
-        .log('starting')
+        .log('Starting')
         .waitForVisible('[data-hook="page-container"] .page')
+        .confirm('The app loaded to the login page.')
+        .confirm('I can log in with Facebook.')
+        .confirm('I can log out.')
+        .confirm('I can in with an email and password.')
+        .waitForVisible('[data-hook="featured"]')
         .confirm('The app loaded to the home page.')
-        .confirm('The "home" nav tab is active')
-        .goToPage('/collections')
-        .confirm('Collection demo page visible')
-        .confirm('List of people are visible each with avatars')
-        .instruct('Five users will be added. Please ensure.', [
-            'each one is added at the bottom of the list',
-            'each has a readable name an avatar'
+        .confirm('Featured gifts are visible.')
+        .confirm('Most popular gifts are visible.')
+        .instruct('A gift image will be clicked. Please ensure:', [
+            'A gift detail page loads'
         ])
-        .click('[data-hook="add"]')
-        .click('[data-hook="add"]')
-        .click('[data-hook="add"]')
-        .click('[data-hook="add"]')
-        .click('[data-hook="add"]')
+        .click('[data-hook="image"]')
+        .waitForVisible('[data-hook="image"]')
         .confirm('Everything look ok?')
-        .confirm('I can visually re-arrange them by pressing, "shuffle"')
-        .confirm('I can hit reset and they disappear, and fetch and they come back.')
-        .confirm('I can delete them by clicking "delete"')
-        .goToPage('/info')
-        .confirm('Info page is visible')
+        .goToPage('/admin')
+        .confirm('Admin page is visible')
         .destroy();
 });
